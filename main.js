f4187b27f12cfc75f9e99c4198beb97b3f8a66e2
bonniiez <30392206+bonniiez@users.noreply.github.com>
2017-08-29 09:43:09 -0700

    html/js files

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// document.querySelector('html').onclick=function(){
+// 	alert('poke!');
+// }
+
+// var myImage = document.querySelector('img');
+// myImage.onclick = function(){
+// 	var mySrc = myImage.getAttribute('src');
+// 	if (mySrc === 'images/firefox-icon.png'){
+// 		myImage.setAttribute('src', 'images/firefox2.png');
+// 	}	else {
+// 		myImage.setAttribute('src', images/firefox-icon.png);
+// 	}
+// }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1')
+
+function setUserName(){
+	var myName = prompt('Please enter your name');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Mozilla is cool,' + myName;
+}
+
+if (localStorage.getItem('name')){ // if name data exists
+	var storedName = localStorage.getItem('name');		// if name exists,
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+} else {
+	setUserName();
+}
+
+myButton.onclick = function(){
+	setUserName();
+}
